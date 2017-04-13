aeac26213f9e6caf619f6d0f16ffb833c4d25abc
Jordan Drake <JoDra13579@gmail.com>
2017-04-13 11:12:27 -0400

    Added no-arg and parametrized constructors and fields to the class

--- src/PostOffice.java
+++ src/PostOffice.java
@@ -10,5 +10,35 @@
  */
 public class PostOffice
 {
+    double lat,
+           lon;
+    int    zip;
+    String city;
     
+    /**
+     * No-arg constructor
+     */
+    public PostOffice()
+    {
+        lat  = 43.55660;
+        lon  = -83.99430;
+        zip  = 48640;
+        city = "University Center";
+    }
+    
+    /**
+     * Parametrized constructor accepting latitude, longitude, zip code, and
+     * the city of the post office object
+     * @param lat  Latitude in decimal degrees
+     * @param lon  Longitude in decimal degrees
+     * @param zip  ZIP code
+     * @param city City name
+     */
+    public PostOffice( double lat, double lon, int zip, String city )
+    {
+        this.lat  = lat;
+        this.lon  = lon;
+        this.zip  = zip;
+        this.city = city;
+    }
 }
