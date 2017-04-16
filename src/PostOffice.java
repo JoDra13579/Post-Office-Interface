c46a95c1938e1a5924d1078bcc415c4105fd3c7a
Jordan Drake <JoDra13579@gmail.com>
2017-04-16 17:24:39 -0400

    Added descriptive comment to class

--- src/PostOffice.java
+++ src/PostOffice.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Doctor
+/** -Jordan Drake-CST183-Tim Klingler-
+ * This class contains information about a Post Office including the 
+ * location (using latitude and longitude), the corresponding city,
+ * and its ZIP code. It also contains an instance method to calculate
+ * the distance to another PostOffice object.
+ * @author Jordan Drake
  */
 public class PostOffice
 {
