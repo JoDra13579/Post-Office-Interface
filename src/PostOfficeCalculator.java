e3770fcc1de12655941fd8044d21014aa94b8487
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 13:22:39 -0400

    Changed layout from GridLayout to BorderLayout to allow flexible sizing

--- src/PostOfficeCalculator.java
+++ src/PostOfficeCalculator.java
@@ -7,24 +7,54 @@
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class PostOfficeCalculator extends JFrame
 {
+    private final int WIDTH = 200,
+                     HEIGHT = 250;
+    
     private ButtonsPanel keypad;
     private ZipField zipBox;
+    private JPanel exitPanel;
+    private JButton exit;
     
     private PostOfficeCalculator()
     {
-        this.setLayout( new GridLayout( 2, 1, 10, 0 ) );
+        this.setTitle( "Post Office Search" );
+        
+        this.setSize( WIDTH, HEIGHT );
+        
+        JOptionPane.showMessageDialog(this, "Search for a post office by ZIP "
+                + "code using your keypad or the on-screen keypad");
+        this.setLayout( new BorderLayout( 0, 10 ) );
         zipBox = new ZipField();
         zipBox.setEditable( false );
-        this.add( zipBox );
+        this.add( zipBox, BorderLayout.NORTH );
         
-        // Create new keypad associated with the zip code box
+        // Create new keypad associated with the zip code box and add to layout
         keypad = new ButtonsPanel( zipBox );
+        this.add( keypad, BorderLayout.CENTER );
+        
+        // Create exitPanel, add Exit button, and add panel to layout
+        exitPanel = new JPanel();
+        exit = new JButton( "Exit" );
+        exit.addActionListener( new ExitListener() );
+        exitPanel.add( exit );
+        this.add( exitPanel, BorderLayout.SOUTH );
+        
+        this.setVisible( true );
+        
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     }
     
-    
+    private class ExitListener implements ActionListener
+    {
+        public void actionPerformed( ActionEvent e )
+        {
+            System.exit( 0 );
+        }
+    }
 
     /**
      * @param args the command line arguments
