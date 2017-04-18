28d7df873d60f60c1a111c42c3916f52f96d10af
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 14:49:01 -0400

    Added search method and altered constructor to set center

--- src/POList.java
+++ src/POList.java
@@ -2,9 +2,7 @@
 /**
  * This class contains an array of <code>PostOffice</code> objects and a single
  * separate <code>PostOffice</code> object that is considered the center for the
- * other <code>PostOffice</code> objects. It also contains a method to calculate
- * and return an array (parallel to the <code>PostOffice</code> array) of
- * distances to the <code>center</code>.
+ * other <code>PostOffice</code> objects.
  * @author Jordan Drake
  */
 
@@ -20,10 +18,11 @@ public class POList
      * Parameterized constructor 
      * @param listFile     The <code>File</code> object to read the post office
      *                     information from
+     * @param center       The <code>PostOffice</code> object to use as the <
      * @throws IOException If the file cannot be found of there is an error
      *                     reading from the file
      */
-    public POList( File listFile ) throws IOException
+    public POList( File listFile, PostOffice center ) throws IOException
     {
         Scanner listIn = new Scanner( listFile );
 
@@ -42,6 +41,8 @@ public class POList
             poList[ i++ ] = new PostOffice( listIn.nextInt(),
                     listIn.nextDouble(), listIn.nextDouble(),
                     listIn.nextLine().replace( '_', ' ' ) );
+        
+        this.center = new PostOffice( center );
     }
     
     /**
@@ -54,5 +55,30 @@ public class POList
         
         for ( int i = 0; i < list.poList.length; i++ )
             this.poList[ i ] = new PostOffice( list.poList[ i ] );
+        
+        this.center = new PostOffice( list.center );
+    }
+    
+    /**
+     * Searches <code>poList</code> for a <code>PostOffice</code> with the zip
+     * code <code>zip</code> and if it finds one, returns it
+     * @param zip The zip code to search for in <code>poList</code>
+     * @return The <code>PostOffice</code> matching <code>zip</code>, unless 
+     *         <code>zip</code> matches <code>center</code> or <code>zip</code>
+     *         is not found, then <code>null</code> will be returned.
+     */
+    public PostOffice searchZip ( int zip )
+    {
+        // Return null if zip matches center
+        if ( zip == center.getZip() )
+            return null;
+        
+        // If zip is found in poList, return that PostOffice object
+        for ( PostOffice p : poList )
+            if ( zip == p.getZip() )
+                return new PostOffice( p );
+        
+        // Otherwise return null
+        return null;
     }
 }
