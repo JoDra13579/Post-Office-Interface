9a7651b180d3b5a039d8078078a5d1bf3e32f267
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 14:47:33 -0400

    Added constructor to the class to populate its list field

--- src/ZipField.java
+++ src/ZipField.java
@@ -4,13 +4,38 @@
  */
 
 import javax.swing.*;
+import java.io.*;
+
 public class ZipField extends JTextField
 {
+    private POList list;
+    
+    /**
+     * Constructor accepting a <code>listFile</code> to create the 
+     * <code>POList</code> for searching
+     * @param listFile
+     * @throws IOException If the file cannot be found of there is an error
+     *                     reading from the file
+     */
+    public ZipField( File listFile ) throws IOException
+    {                                                    // Use default
+        list = new POList( listFile, new PostOffice() ); // constructor to make
+    }                                                    // center
+    
+    /**
+     * Method to add numbers to the <code>ZipField</code> (only up to 5 numbers)
+     * @param j The <code>int</code> to append to the contents of the 
+     *          <code>ZipField</code>
+     */
     public void type( int j )
     {
-        this.setText( this.getText() + j );
+        if ( this.getText().length() < 5 )
+            this.setText( this.getText() + j );
     }
     
+    /**
+     * Method to clear the contents of the <code>ZipField</code>
+     */
     public void clear()
     {
         this.setText( "" );
@@ -18,6 +43,6 @@ public class ZipField extends JTextField
     
     public void submit()
     {
-        
+        list.searchZip( Integer.parseInt( this.getText() ) );
     }
 }
