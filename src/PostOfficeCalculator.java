bb68c599e975ff12e457cb9fa962836cc9009fea
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 16:13:28 -0400

    Added search method and fixed various other syntax errors

--- src/PostOfficeCalculator.java
+++ src/PostOfficeCalculator.java
@@ -13,15 +13,20 @@ import java.io.*;
 public class PostOfficeCalculator extends JFrame
 {
     // ***CONSTANTS***
-    private final int       WIDTH = 200,
-                           HEIGHT = 250;
-    private final String FILENAME = "zipMIcity.txt";
+    private static final int         WIDTH = 200,
+                                    HEIGHT = 250;
+    private static final File     LISTFILE = new File( "zipMIcity.txt" );
+    private static final PostOffice CENTER = new PostOffice( 48640, 43.55660, 
+            -83.99430, "University Center" );
     
     // **GUI Components**
     private ButtonsPanel keypad;
     private ZipField zipBox;
     private JPanel exitPanel;
     private JButton exit;
+    
+    private POList list;
+    private PostOffice result;
     
     private PostOfficeCalculator()
     {
@@ -35,20 +40,12 @@ public class PostOfficeCalculator extends JFrame
         this.setLayout( new BorderLayout( 0, 10 ) );
         
         // Create textfield and add it to layout
-        try
-        {
-            zipBox = new ZipField( new File( FILENAME ) );
-        }
-        catch ( IOException e )
-        {
-            JOptionPane.showMessageDialog( this, "Error:" + e.getMessage(),
-                    "File error", JOptionPane.ERROR_MESSAGE );
-        }
+        zipBox = new ZipField();
         zipBox.setEditable( false );
         this.add( zipBox, BorderLayout.NORTH );
         
         // Create new keypad associated with the zip code box and add to layout
-        keypad = new ButtonsPanel( zipBox );
+        keypad = new ButtonsPanel( zipBox, this );
         this.add( keypad, BorderLayout.CENTER );
         
         // Create exitPanel, add Exit button, and add panel to layout
@@ -62,11 +59,34 @@ public class PostOfficeCalculator extends JFrame
         this.setVisible( true );
         
         // Show informational dialog
-        JOptionPane.showMessageDialog(this, "Search for a post office by ZIP "
-                + "code using your keypad or the on-screen keypad");
+        JOptionPane.showMessageDialog( this, "Search for a post office by ZIP "
+                + "code using your keypad or the on-screen keypad" );
         
         // Set program to close on window exit
         this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        
+        
+        try
+        {
+            list = new POList( LISTFILE, CENTER );
+        }
+        catch ( IOException e )
+        {
+            JOptionPane.showMessageDialog( this, "Error: " + e.getMessage(), 
+                    "File Error", JOptionPane.ERROR_MESSAGE);
+            System.exit( 0 );
+        }
+        
+    }
+    
+    public void search( int zip )
+    {
+        result = list.searchZip( zip );
+        String outStr = "";
+        outStr += result;
+        outStr += String.format( "%.2f miles from %d in %s", 
+                result.distanceTo(CENTER), result.getZip(), result.getCity() );
+        JOptionPane.showMessageDialog( this, outStr);
     }
     
     private class ExitListener implements ActionListener
