4cb045218868c0f738488149e754fc6212e85873
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 13:02:33 -0400

    Added GUI components

--- src/PostOfficeCalculator.java
+++ src/PostOfficeCalculator.java
@@ -1,16 +1,30 @@
+//-CST183-Tim Klingler-
 /**
- * This class extends the JFrame class and controls a window for interfacing with
- * an object of the POList class
+ * This class extends the <code>JFrame</code> class and controls a window for
+ * interfacing with an object of the <code>POList</code> class
  * @author Jordan Drake
  */
 
+import java.awt.*;
+import javax.swing.*;
+
 public class PostOfficeCalculator extends JFrame
 {
+    private ButtonsPanel keypad;
+    private ZipField zipBox;
     
-    public PostOfficeCalculator()
+    private PostOfficeCalculator()
     {
+        this.setLayout( new GridLayout( 2, 1, 10, 0 ) );
+        zipBox = new ZipField();
+        zipBox.setEditable( false );
+        this.add( zipBox );
         
+        // Create new keypad associated with the zip code box
+        keypad = new ButtonsPanel( zipBox );
     }
+    
+    
 
     /**
      * @param args the command line arguments
