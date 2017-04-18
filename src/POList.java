b1e8b3497ad7a038767e49a39a02f1c6926bb52f
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 11:12:01 -0400

    Added <code> tags in Javadoc where appropriate and corrected typos

--- src/POList.java
+++ src/POList.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 import java.io.*;
 
-/** -Jordan Drake-CST183-Tim Klingler-
- * This class contains an array of PostOffice objects and a single separate
- * PostOffice object that is considered the center for the other PostOffice
- * objects. It also contains a method to calculate and return an array (parallel
- * to the PostOffice array) of discances to the "center."
+/**-CST183-Tim Klingler-
+ * This class contains an array of <code>PostOffice</code> objects and a single
+ * separate <code>PostOffice</code> object that is considered the center for the
+ * other <code>PostOffice</code> objects. It also contains a method to calculate
+ * and return an array (parallel to the <code>PostOffice</code> array) of
+ * distances to the <code>center</code>.
  * @author Jordan Drake
  */
 
 public class POList
 {
     private PostOffice[] poList;
-	private PostOffice   center;
+    private PostOffice   center;
 	
     /**
      * Parameterized constructor 
-     * @param listFile     The file object to read the post office information
-	 *                     from
+     * @param listFile     The <code>File</code> object to read the post office
+     *                     information from
      * @throws IOException If the file cannot be found of there is an error
      *                     reading from the file
      */
@@ -43,8 +44,8 @@ public class POList
     }
     
     /**
-     * Copy constructor for POList objects
-     * @param list The POList object to copy
+     * Copy constructor for <code>POList</code> objects
+     * @param list The <code>POList</code> object to copy
      */
     public POList( POList list )
     {
