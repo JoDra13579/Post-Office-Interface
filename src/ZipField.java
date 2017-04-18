b3b5ac9ea50e39335a0d09194c327ef9824c18ac
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 16:11:46 -0400

    Moved submit() method to driver class

--- src/ZipField.java
+++ src/ZipField.java
@@ -8,20 +8,6 @@ import java.io.*;
 
 public class ZipField extends JTextField
 {
-    private POList list;
-    
-    /**
-     * Constructor accepting a <code>listFile</code> to create the 
-     * <code>POList</code> for searching
-     * @param listFile
-     * @throws IOException If the file cannot be found of there is an error
-     *                     reading from the file
-     */
-    public ZipField( File listFile ) throws IOException
-    {                                                    // Use default
-        list = new POList( listFile, new PostOffice() ); // constructor to make
-    }                                                    // center
-    
     /**
      * Method to add numbers to the <code>ZipField</code> (only up to 5 numbers)
      * @param j The <code>int</code> to append to the contents of the 
@@ -39,10 +25,5 @@ public class ZipField extends JTextField
     public void clear()
     {
         this.setText( "" );
-    }
-    
-    public void submit()
-    {
-        list.searchZip( Integer.parseInt( this.getText() ) );
     }
 }
