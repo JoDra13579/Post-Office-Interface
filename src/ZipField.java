292a7a286eda89f09524fec86deb4619a329268d
Jordan Drake <JoDra13579@gmail.com>
2017-04-18 13:03:32 -0400

    Added extending methods for the class

--- src/ZipField.java
+++ src/ZipField.java
@@ -1,10 +1,23 @@
 /**
  *
- * @author Doctor
+ * @author Jordan Drake
  */
 
 import javax.swing.*;
 public class ZipField extends JTextField
 {
+    public void type( int j )
+    {
+        this.setText( this.getText() + j );
+    }
     
+    public void clear()
+    {
+        this.setText( "" );
+    }
+    
+    public void submit()
+    {
+        
+    }
 }
